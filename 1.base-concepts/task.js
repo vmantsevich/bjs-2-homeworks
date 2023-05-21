837c3b6ad63cefe88bde8343b91d8a9b31888265
VMantsevich <vladislava_mantsevich@mail.ru>
2023-05-21 18:46:13 +0300

    1st task corrected

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
-var arr = []; 
+let arr = []; 
 let d = b**2 - 4*a*c;
 if (d < 0) {
   alert(arr);
-} if (d == 0) {
+} if (d === 0) {
    let root3 = -b / (2*a);
-  alert(root3);
+  arr.push(root3);
 } if (d > 0) {
   let root1 = (-b + Math.sqrt(d)) / (2*a);
   let root2 = (-b - Math.sqrt(d)) / (2*a);
-  alert(root1, root2);
-};
-};
-const arr=[-6, 9, 2];
+  arr.push(root1, root2);
+}
+  return arr;
+}
+const arr=[5, 2, -7];
 solveEquation.apply(null, arr);
+
 //значения d<0 (-6, 9, 2)
 //значения d>0 (5, 2, -7)
 //значения d=0 (16, -8, 1)
