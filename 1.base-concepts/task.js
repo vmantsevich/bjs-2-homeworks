ecc7534c84d417260f116660200fab2e17a8c14c
VMantsevich <vladislava_mantsevich@mail.ru>
2023-05-21 13:37:49 +0300

    1st task ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+var arr = []; 
+let d = b**2 - 4*a*c;
+if (d > 0) {
+  let root1 = (-b + Math.sqrt(d)) / (2*a);
+  let root2 = (-b - Math.sqrt(d)) / (2*a);
+  alert("Имеет два значения: " + "Первый корень: " + root1 + " Второй корень: " + root2);
+} if (d < 0) {
+  alert(arr);
+} if (d == 0) {
+   let root3 = -b / (2*a);
+  alert("Имеет одно значение: " + root3);
+};
+};
+const arr=[-6, 9, 2];
+solveEquation.apply(null, arr);
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+//значения d<0 (-6, 9, 2)
+//значения d>0 (5, 2, -7)
+//значения d=0 (16, -8, 1)
