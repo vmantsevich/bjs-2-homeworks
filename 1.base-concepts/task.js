690b5c605a66c1474d80d53af4fb83aadc558959
VMantsevich <vladislava_mantsevich@mail.ru>
2023-05-29 17:53:46 +0300

    2nd tasc corrected

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,7 +3,7 @@ function solveEquation(a, b, c) {
 let arr = []; 
 let d = b**2 - 4*a*c;
 if (d < 0) {
-  alert(arr);
+  return arr;
 } if (d === 0) {
    let root3 = -b / (2*a);
   arr.push(root3);
@@ -22,12 +22,24 @@ solveEquation.apply(null, arr);
 //значения d=0 (16, -8, 1)
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (!isNaN(percent) && !isNaN(contribution) && !isNaN(amount) && !isNAN(countMonths)) {
-      const perMonth = percent / 100 / 12;
-      const bodyCredit = amount - contribution;
-      const monthly = bodyCredit * (perMonth + (perMonth / (((1 + perMonth) ** countMonths) - 1)));
-      const totalSum = parseFloat((monthly * countMonths).toFixed(2));
-      return totalSum;
+  if (typeof percent !== "number") {
+    return `Процентная ставка содержит неправильное значение "${percent}"`;
+  } 
+  if (typeof contribution !== "number") {
+    return `Сумма первоначального взноса содержит неправильное значение "${contribution}"`;
+  } 
+  if (typeof amount !== "number") {
+    return `Сумма кредита содержит неправильное значение "${amount}"`;
+  } 
+  if (typeof countMonths !== "number") {
+    return `Срок содержит неправильное значение "${countMonths}"`;
   }
-  return false;
+ 
+
+const perMonth = percent / 100 / 12;
+const bodyCredit = amount - contribution;
+const monthly = bodyCredit * (perMonth + (perMonth / (((1 + perMonth) ** countMonths) - 1)));
+const totalSum = parseFloat((monthly * countMonths).toFixed(2));
+return totalSum;
 }
+console.log(calculateTotalMortgage(10, 0, 20000, 24));
