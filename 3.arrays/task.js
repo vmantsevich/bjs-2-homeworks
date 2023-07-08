980aee1a35a8e149f1ccc2ba7dcaa055f9917e8b
VMantsevich <vladislava_mantsevich@mail.ru>
2023-07-08 20:04:16 +0300

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.every((item, index) => arr1.length === arr2.length && item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender).reduce((averAge, userAge, index, array) => averAge + userAge.age / array.length, 0);
+	return result
 }
