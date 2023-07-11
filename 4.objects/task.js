5296eba6a0a9c4d2956237296e706b3a3f8a3490
VMantsevich <vladislava_mantsevich@mail.ru>
2023-07-11 18:21:09 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
+   
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
+   
+  Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.excluded) {
+      console.log(`Студент ${this.name} отчислен. Добавление оценок невозможно.`);
+      return;
+    }
+    this.marks.push(...marksToAdd);
+  }
+   
+  Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    }
+    const sum = this.marks.reduce(function (accumulator, currentValue) {
+      return accumulator + currentValue;
+    });
+    return +(sum / this.marks.length).toFixed(1);
+  }
+   
+  Student.prototype.exclude = function (reason) {
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
+  };
